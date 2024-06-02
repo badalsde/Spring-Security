dfdf1dac2acfc732c655c08f9597b3f17deb0745
Badal Kumar <bk82623@gmail.com>
2024-06-03 01:12:30 +0530

    Init Commit

--- 04.BasicOauth2.0/src/main/java/com/oauth/config/SpringSecurityConfig.java
+++ 04.BasicOauth2.0/src/main/java/com/oauth/config/SpringSecurityConfig.java
@@ -5,18 +5,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig {
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests)->requests
-                .anyRequest()
-                .authenticated())
+        http.authorizeHttpRequests((requests) -> requests
+                        .anyRequest().authenticated())
                 .oauth2Login(Customizer.withDefaults());
-
+       // http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.csrf(AbstractHttpConfigurer::disable);
         return http.build();
     }
 }
--- 04.BasicOauth2.0/src/main/java/com/oauth/controller/OauthSecurityController.java
+++ 04.BasicOauth2.0/src/main/java/com/oauth/controller/OauthSecurityController.java
@@ -1,15 +1,36 @@
 package com.oauth.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.security.Principal;
-
-@RestController
+@Controller
 public class OauthSecurityController {
 
-    @GetMapping("/auth")
-    public String getRequest(Principal user) {
+    @GetMapping("/")
+    public String getRequest(Model model, Authentication authentication) {
+        if (authentication != null && authentication.getPrincipal() instanceof OAuth2User) {
+            OAuth2User oauth2User = (OAuth2User) authentication.getPrincipal();
+
+            // Get the user's Name
+            String userName = oauth2User.getAttribute("name");
+            model.addAttribute("userName", userName);
+
+            // Get the user's email address
+            String email = oauth2User.getAttribute("email");
+            model.addAttribute("email", email);
+
+            // Get the user's gender (if available)
+            String gender = oauth2User.getAttribute("gender");
+            model.addAttribute("gender", gender);
+        }
+        // Add model attributes if needed
+        model.addAttribute("message", "Hello, welcome to our application!");
+
+        // Return the name of the Thymeleaf template without the file extension
         return "welcome";
     }
 }
